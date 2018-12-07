2b7919f78fbbbcd67d7338a499ecaed8e5893ce4
Toni de la Torre <dlt@erni-espana.es>
2018-12-07 15:51:33 +0100

    Add project files.

---
+++ GetApp_Import/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetApp_Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
